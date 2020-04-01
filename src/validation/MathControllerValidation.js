6f03f9e5142c3036052415e3fc1d1d8909d2204a
Tim van Dam <timvandamcs@gmail.com>
2020-04-01 23:09:04 +0200

    now casts param (string) to number

--- src/validation/MathControllerValidation.js
+++ src/validation/MathControllerValidation.js
@@ -1,5 +1,7 @@
 module.exports = (req, res, next) => {
   const { a, b } = req.params
   if (!a || !b || isNaN(a) || isNaN(b)) return res.status(400).send('Invalid parameters')
+  req.params.a = parseInt(a)
+  req.params.b = parseInt(b)
   next()
 }
